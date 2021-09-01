ff554212066a5eafb0f08f2353a1ba80437ba83e
thien <thien.nd01@pnj.com.vn>
2021-09-02 00:35:43 +0700

    update app name

--- contract/target/wasm32-unknown-unknown/debug/greeter.d
+++ contract/target/wasm32-unknown-unknown/debug/greeter.d
@@ -1,1 +1,1 @@
-/Users/mac/Desktop/home/near-pay/payapp/contract/target/wasm32-unknown-unknown/debug/greeter.wasm: /Users/mac/Desktop/home/near-pay/payapp/contract/src/lib.rs
+/Users/mac/Desktop/home/near-pay/windpay/contract/target/wasm32-unknown-unknown/debug/greeter.wasm: /Users/mac/Desktop/home/near-pay/windpay/contract/src/lib.rs
--- contract/target/wasm32-unknown-unknown/debug/libgreeter.d
+++ contract/target/wasm32-unknown-unknown/debug/libgreeter.d
@@ -1,1 +1,1 @@
-/Users/mac/Desktop/home/near-pay/payapp/contract/target/wasm32-unknown-unknown/debug/libgreeter.rlib: /Users/mac/Desktop/home/near-pay/payapp/contract/src/lib.rs
+/Users/mac/Desktop/home/near-pay/windpay/contract/target/wasm32-unknown-unknown/debug/libgreeter.rlib: /Users/mac/Desktop/home/near-pay/windpay/contract/src/lib.rs
